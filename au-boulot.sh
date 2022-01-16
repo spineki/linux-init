77b5fc31d1d358bc9d96ef78725c985e44a5df58
Antoine Marras <57460188+spineki@users.noreply.github.com>
2022-01-16 21:18:14 +0100

    Create au-boulot.sh

---
+++ au-boulot.sh
@@ -0,0 +1,5 @@
+open /Applications/Telegram.app &
+open /Applications/Slack.app &
+open "/Applications/Visual Studio Code.app" --args -r &
+open /Applications/Firefox.app --args "https://mail.google.com/mail/u/0/?tab=wm#inbox" "https://trello.com/b/MH3ybZzS/songpop-3" &
+open /Applications/Spotify.app &
